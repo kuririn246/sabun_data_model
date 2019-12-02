8a70e1c08e63eeaac9d41e44c1f7234a87996358
massive-spoiler <reddit_chan@outlook.jp>
2019-12-02 17:50:38 +0900

    データ使用量のシミュレーションをしようと思う。 計算量よりもデータの小ささで律速されるはずなので頑張って比較して最小を目指すべきだろう。

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,63 @@
-fn main() {
+use std::iter::repeat;
 
-    let moto = 10.0;
-    let shin = 5.0;
-    let b_sabun = 50.0;
+struct StartFrom{
+    index : i32,
+    vec : Vec<i32>
+}
+
+struct CSabuns{
+    vec : Vec<StartFrom>
+}
+
+struct Sabuns{
+    start_index : i32,
+    initial : Vec<i32>,
+    a_sabun : Vec<StartFrom>,
+    b_sabun : Vec<StartFrom>,
+    c_sabun : Vec<CSabuns>,
+    last_sabun : String,
+}
+
+fn calc_sabuns(sabuns : &mut Sabuns, current : &Vec<i32>){
+    let last_sabun = match sabuns.last_sabun.as_str(){
+        "i" => 0,
+        "a" => 1,
+        "b" => 2,
+        "c" => 3,
+        _ => -1,
+    };
+}
+
+fn main() {
+//    fn last_index(vec : &Vec<StartFrom>) -> i32{
+//        let last = vec.last().unwrap();
+//        return last.index + last.vec.len();
+//    }
+
+    let first = StartFrom{
+        index : 0,
+        vec : vec![0; 100]
+    };
+
+    let mut current = first.vec.clone();
+
+    let mut vec : Vec<StartFrom> = vec![];
+    vec.push(first);
+
+    //データが一個ずつふえ、rewrite個のデータがrewriteされる。
+    //rewriteする数は一定で、かならず配列の最後の部分だけrewriteする。
+    //なので変化しない部分も一つずつ増えていく。
+    let increase = 1;
+    let rewrite = 10;
+
     for n in 1..100{
-        let mut turn = n as f64;
-        let current = (b_sabun * 2.0 + shin * turn) / (turn + 2.0);
-        let next = ((b_sabun * 2.0 + shin * (turn + 1.0)) + moto) / (turn + 4.0);
-        let big = if current < next{ "big" } else { "small" };
-        println!("{} {} {} {}", big, turn, current, next);
+        current.push(n);
+        let start = current.len() - rewrite;
+        for i in start..current.len(){
+            current[i] = n;
+        }
+
+
     }
 
 
