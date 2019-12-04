5a6dbf0a2ede2924904e0d1594438d3d7ce2e616
massive-spoiler <reddit_chan@outlook.jp>
2019-12-04 16:27:04 +0900

    シミュレーション用のコード、コンパイル通す

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ impl StartFrom{
     }
 }
 
+#[derive(Debug)]
+enum Sabun{
+    Initial, A,B,C
+}
+
 struct ASabun{
     sabun : StartFrom,
     b_sabuns : Vec<BSabun>
@@ -31,7 +36,7 @@ struct Sabuns{
     start_index : usize,
     initial : Vec<i32>,
     a_sabuns : Vec<ASabun>,
-    last_sabun : String,
+    last_sabun : Sabun,
 }
 
 impl Sabuns{
@@ -73,19 +78,17 @@ fn apply(vec : &mut Vec<i32>, sabun : &StartFrom){
 }
 
 
-fn construct(last_sabun : &str, sabuns : &Sabuns) -> Vec<i32>{
+fn construct(last_sabun : &Sabun, sabuns : &Sabuns) -> Vec<i32>{
     let last_sabun = match last_sabun{
-        "i" => 0,
-        "a" => 1,
-        "b" => 2,
-        "cb" => 3,
-        "cp" => 4,
-        _ => -1,
+        Sabun::Initial => 0,
+        Sabun::A => 1,
+        Sabun::B => 2,
+        Sabun::C => 3,
     };
 
     let mut vec = sabuns.initial.clone();
     if last_sabun <= 1 {
-        let a = sabuns.a_sabuns.last().unwrap();
+        let a = sabuns.get_a();
         apply(&mut vec, &a.sabun);
 
         if last_sabun <= 2 {
@@ -94,12 +97,8 @@ fn construct(last_sabun : &str, sabuns : &Sabuns) -> Vec<i32>{
 
             if last_sabun <= 3{
                 let c = b.c_sabuns.last().unwrap();
-                apply(&mut vec, &c.vec[0]);
-
-                if last_sabun <= 4{
-                    for item in &c.vec[1..]{
-                        apply(&mut vec, item);
-                    }
+                for item in &c.vec{
+                    apply(&mut vec, item);
                 }
             }
         }
@@ -111,7 +110,7 @@ fn make_sabun(current : &Vec<i32>, prev : &Vec<i32>) -> StartFrom {
     for i in 0..current.len() {
         if i < prev.len() {
             if current[i] != prev[i] {
-                return StartFrom { vec: (&current[n..]).to_vec(), index: i }
+                return StartFrom { vec: (&current[i..]).to_vec(), index: i }
             }
         } else { break; }
     }
@@ -121,15 +120,15 @@ fn make_sabun(current : &Vec<i32>, prev : &Vec<i32>) -> StartFrom {
 }
 
 fn c_is_full(sabuns : &Sabuns) -> bool{
-    return sabuns.c_sabuns.last().unwrap().vec.len() > 3;
+    return sabuns.get_c().vec.len() > 3;
 }
 
 fn calc_common_size(a : &StartFrom, b : &StartFrom) -> usize{
     let start = max(a.index, b.index);
     let end = min(a.index + a.vec.len(), b.index + b.vec.len());
     for n in start..end{
-        a_ind = n - a.index;
-        b_ind = n - b.index;
+        let a_ind = n - a.index;
+        let b_ind = n - b.index;
         if a.vec[a_ind] != b.vec[b_ind]{
             return n - start;
         }
@@ -141,7 +140,7 @@ fn should_update_b(current : &Vec<i32>, sabuns : &Sabuns) -> bool{
 
     let prev_cb_sabun = &sabuns.get_c().vec[0];
 
-    let b_data = construct("b", sabuns);
+    let b_data = construct(&Sabun::B, sabuns);
     let new_cb_sabun = &make_sabun(current, &b_data);
 
     let common_size = calc_common_size(prev_cb_sabun, new_cb_sabun);
@@ -149,27 +148,68 @@ fn should_update_b(current : &Vec<i32>, sabuns : &Sabuns) -> bool{
     ///new_cb_sabunのうちprev_cb_sabunとの共通部分がどれだけあるか調べる
     ///その共通部分はBをアップデートすればそこに取り込まれ続くファイルのサイズを減らすことができるので、
     ///それを引いてアップデート後の新サイズを推測する
-    let guessed_cb_size = new_cb_sabun.size() as i32 - common_size as i32;
+    let guessed_cb_size = new_cb_sabun.size() - common_size;
 
-    let b_sabun = sabuns.b_sabuns.last().unwrap();
+    let b_sabun = sabuns.get_b();
 
-    let c = sabuns.get_b().c_sabuns.len();
+    let n = b_sabun.c_sabuns.len();
 
-    4 * b_sabun.size() - 2 * (new_cb_sabun.size() + guessed_cb_size) < c * (new_cb_sabun.size() - guessed_cb_size)
+    4 * b_sabun.sabun.size() - 2 * (new_cb_sabun.size() + guessed_cb_size) < n * (new_cb_sabun.size() - guessed_cb_size)
     //4A - 2(B' + B) < n(B' - B)
 }
 
-fn try_push_c_sabun(sabuns : &mut Sabuns, current : Vec<i32>){
+fn should_update_a(current : &Vec<i32>, sabuns : &Sabuns) -> bool{
+
+    let prev_b_sabun = &sabuns.get_b().sabun;
+
+    let a_data = construct(&Sabun::A, sabuns);
+    let new_b_sabun = &make_sabun(current, &a_data);
+
+    let common_size = calc_common_size(prev_b_sabun, new_b_sabun);
+
+    ///new_cb_sabunのうちprev_cb_sabunとの共通部分がどれだけあるか調べる
+    ///その共通部分はBをアップデートすればそこに取り込まれ続くファイルのサイズを減らすことができるので、
+    ///それを引いてアップデート後の新サイズを推測する
+    let guessed_b_size = new_b_sabun.size() - common_size;
+
+    let a_sabun = sabuns.get_a();
+
+    let n = a_sabun.b_sabuns.len();
+
+    4 * a_sabun.sabun.size() - 2 * (new_b_sabun.size() + guessed_b_size) < n * (new_b_sabun.size() - guessed_b_size)
+    //4A - 2(B' + B) < n(B' - B)
+}
+
+fn try_push_c_sabun(sabuns : &mut Sabuns, current : &Vec<i32>){
     if !c_is_full(sabuns){
-        let prev = construct("cp", sabuns);
-        let last_sabun = make_sabun(&current, &prev);
+        let prev = construct(&Sabun::C, sabuns);
+        let last_sabun = make_sabun(current, &prev);
         sabuns.get_c_mut().vec.push(last_sabun);
+        return;
     }
     else{
        if should_update_b(&current, sabuns){
-
+            if should_update_a(&current, sabuns){
+                let sabun = make_sabun(current, &sabuns.initial);
+                sabuns.a_sabuns.push(ASabun{ sabun, b_sabuns : vec![] });
+                sabuns.last_sabun = Sabun::A;
+                return;
+            }
+           else{
+               let prev = construct(&Sabun::A, sabuns);
+               let sabun = make_sabun(current, &prev);
+               sabuns.get_a_mut().b_sabuns.push(BSabun{ sabun, c_sabuns : vec![] });
+               sabuns.last_sabun = Sabun::B;
+               return;
+           }
        }
-
+       else{
+           let prev = construct(&Sabun::B, sabuns);
+           let sabun = make_sabun(current, &prev);
+           sabuns.get_b_mut().c_sabuns.push(CSabuns{ vec: vec![ sabun ]});
+           sabuns.last_sabun = Sabun::C;
+           return;
+       }
     }
 }
 
@@ -177,29 +217,24 @@ fn calc(sabuns : &mut Sabuns, current : &Vec<i32>){
     let prev = construct(&sabuns.last_sabun, sabuns);
 
     let sabun = make_sabun(current, &prev);
-    let last_sabun = sabuns.last_sabun.clone();
-    match last_sabun.as_str(){
-        "i" =>{
+    match &sabuns.last_sabun{
+        &Sabun::Initial =>{
             sabuns.a_sabuns.push(ASabun{ sabun, b_sabuns : vec![] });
-            sabuns.last_sabun = "a".to_string();
+            sabuns.last_sabun = Sabun::A;
         },
-        "a" =>{
-            let a = sabuns.a_sabuns.last_mut().unwrap();
+        &Sabun::A =>{
+            let a = sabuns.get_a_mut();
             a.b_sabuns.push(BSabun{ sabun, c_sabuns : vec![] });
-            sabuns.last_sabun = "b".to_string();
+            sabuns.last_sabun = Sabun::B;
         }
-        "b" =>{
-            let b = sabuns.a_sabuns.last_mut().unwrap().b_sabuns.last_mut().unwrap();
-            b.c_sabuns.push(CSabuns{ vec: vec![ sabun ]};
-            sabuns.last_sabun = "cb".to_string();
+        &Sabun::B =>{
+            let b = sabuns.get_b_mut();
+            b.c_sabuns.push(CSabuns{ vec: vec![ sabun ]});
+            sabuns.last_sabun = Sabun::C;
         }
-        "cb" =>{
-
+        &Sabun::C =>{
+            try_push_c_sabun(sabuns, current);
         }
-        "cp" =>{
-
-        }
-        _=> panic!(),
     }
 }
 
@@ -216,7 +251,7 @@ fn main() {
         initial : current.clone(),
         a_sabuns : vec![],
         start_index : current.len() - rewrite + increase,
-        last_sabun : "i".to_string(),
+        last_sabun : Sabun::Initial,
     };
 
     for n in 1..100{
@@ -225,6 +260,7 @@ fn main() {
         for i in start..current.len(){
             current[i] = n;
         }
+        calc(&mut sabuns, &current)
 
 
 
