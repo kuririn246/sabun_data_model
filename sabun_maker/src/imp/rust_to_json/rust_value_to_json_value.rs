9ee95a1606132064f0d59a2c461d910df22679dc
reddi <reddit_chan@outlook.jp>
2020-03-18 15:36:12 +0900

    きょうはここまで

--- sabun_maker/src/imp/rust_to_json/rust_value_to_json_value.rs
+++ sabun_maker/src/imp/rust_to_json/rust_value_to_json_value.rs
@@ -1,9 +1,19 @@
 use crate::my_json::Value;
-use crate::rust_struct::{RustValue, ValueType};
+use crate::rust_struct::{RustValue, ValueType, Qv};
 
 pub fn rust_value_to_json_value(v : &RustValue) -> (Value, ValueType){
-    match v{
-        RustValue::Bool(b, vt) =>
+    return match v{
+        RustValue::Bool(b, vt) => to(b, vt, "Bool",|b| Value::Bool(*b)),
+        RustValue::String(s, vt) => to(s, vt, "Str", |s| Value::String(s.to_string())),
+        RustValue::Number(n, vt) => to(n, vt, "Num", |n| Value::Num(*n)),
     }
-    todo!()
+
+}
+
+fn to<T>(qv : &Qv<T>, vt : &ValueType, type_name : &str, f : impl Fn(&T)->Value) -> (Value, ValueType){
+    match qv{
+        Qv::Val(v) => (f(v), vt.clone()),
+        Qv::Null => (Value::Array(vec![Value::String(type_name.to_string()), Value::Null]), vt.clone()),
+        Qv::Undefined => (Value::Array(vec![Value::String(type_name.to_string()), Value::Undefined]), vt.clone()),
+    }
 }
