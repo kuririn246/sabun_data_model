e3f4b7ee7ea1e5534172396f9b373969ccca8027
reddi <reddit_chan@outlook.jp>
2020-05-26 07:20:46 +0900

    structを全面変更し、const dataとconst listとmut listの3種類(violated-listも含めて4種類？)にするぞ

--- sabun_maker/src/structs/rust_list.rs
+++ sabun_maker/src/structs/rust_list.rs
@@ -1,6 +1,6 @@
 
 use crate::structs::root_object::{ListDefObj};
-use std::collections::{ HashSet, HashMap};
+use std::collections::{HashSet, HashMap, BTreeMap};
 use linked_hash_map::LinkedHashMap;
 use std::rc::{Rc};
 use crate::indexmap::IndexMap;
@@ -8,54 +8,43 @@ use crate::structs::rust_value::RustValue;
 use crate::structs::ref_value::RefValue;
 
 
-///アイテムごとにIDをもち、Refで参照することが可能である。InnerListとしてはConstList,ConstData,InitialListを持てる。
-/// InnerListでConstDataを使う場合、Refする手段がないので、プログラムからIDでアクセスする使いみちぐらいしかなさそう。
+///アイテムごとにIDをもち、Refで参照することが可能である
 #[derive(Debug, PartialEq)]
 pub struct ConstData{
-    pub default : ListDef,
+    pub default : ListDefObj,
     pub list : IndexMap<String, ListItem>,
     ///oldに設定されたIDはjsonから参照出来ない。変数名の末尾に"_Old"をつけないとプログラムからも使えない。
     pub old : HashSet<String>,
 }
 
-///IDを持たず、参照できない。ConstDataのInnerListで使うのが主な使いみちだけれど、IDはいらないけど単にデータを書いておきたい場合もあるだろう。
-/// InnerListとしてConstData,ConstList,InitialListを持てる。DefaultではDefaultだけを書き、ListItemのInnerListではDefaultをWeakで参照しつつ、ConstなListをその都度書くことになる。
+///IDを持たず、参照できない。MutListの初期値を書くのが主な使い道か。IDは必要ないけど単にデータを書いておきたい場合もあるだろう。
 #[derive(Debug, PartialEq)]
 pub struct ConstList{
-    pub default : ListDef,
+    pub default : ListDefObj,
     pub list : Vec<ListItem>,
-}
-
-///参照できない。MutListにコピーするのが使いみち。InnerListはMutList, MutDataのみ持てる。InitialListの中のMutList,MutDataはListItemを持てる。
-/// InitialListもConstであり、中身も含めて一切変更することは出来ない。
-#[derive(Debug, PartialEq)]
-pub struct InitialList{
-    pub default : ListDef,
-    pub list : Vec<ListItem>,
-    ///MutListは初期値を持てないのでInitialListに初期値を書いておくことになるだろう。
+    ///MutListは初期値を持てないのでConstListに初期値を書いておくことになるだろう。
     /// その場合、compatibleを設定しdefaultが同一であることを保証することで、そのままListItemをコピーすることが可能になる
-    pub compatible : Vec<String>,
+    pub compatible : HashSet<String>,
 }
 
 ///追加、削除、順番の変更等ができるリスト。初期値を持てず最初は必ず空リストである。これはバージョン違いを読み出す時に問題を単純化するために必要。
-///InnerListはMutList,MutDataを持てる。Constのものを内部に抱える必要はなかろうと思う。「生成後変更できないようにしたい」という需要はあるかもしれないが・・・このシステムにとっては必要ではないと思う。
+/// ConstListとMutListはstruct定義を見ると近い存在なので、まとめてもいいように思うかもしれないけれど、意味が全く別ものなので型を分けたほうが混乱が少ない。
+/// 順序を変えなければidでソートされたSortedListになるのでPrimaryKeyを持ったTableとしても使えないこともないか
 #[derive(Debug, PartialEq)]
 pub struct MutList{
-    pub default : ListDef,
+    pub default : ListDefObj,
     pub list : Vec<MutListItem>,
     ///追加される度にこのIDがふられ、これがインクリメントされることを徹底する必要がある。u64を使い切るには1万年ぐらいかかるだろう
     pub next_id : u64,
 }
 
-///追加、削除等できるマップ。必要性はよくわからないが・・・マップもあったほうが良いかという気がするので作っておく。
-///MutのItemに対するRefは、削除されうるので本質的に危険であり、避けたほうがいいように思うが、
-/// たとえばMutで増減するランダムキャラクター同士が好感度をもつような時、キャラクターAに対する好感度をMap(key=A)に入れると効率的に処理できる、といった可能性が考えられる。
-///
-/// MutDataに対して初期値を入れるときは、InitialListのItemからどれをIDにするか、多分nameとか小文字のidとかを設定して、入れることになるだろう。
+///Data or Listの内部に作るList。上がMutの場合はMutだし、ConstならConstだが、アクセス制限の違いだけで中身の違いはない。Constの場合idは無視できる。ListDefObjにはDefaultだけ書き、ListItemでは必要ならItemのみを書く。
+///MutListをTableとして、InnerListをSortedListとして運用することで理論上はRelationも表現できる・・・でも実際はConstDataとRefでやるべきだと思う
 #[derive(Debug, PartialEq)]
-pub struct MutData{
-    pub default : ListDef,
-    pub list : LinkedHashMap<String, ListItem>,
+pub struct InnerList{
+    pub list : Vec<MutListItem>,
+    ///追加される度にこのIDがふられ、これがインクリメントされることを徹底する必要がある。u64を使い切るには1秒間に1億生成しても1万年ぐらいかかるはず
+    pub next_id : u64,
 }
 
 #[derive(Debug, PartialEq)]
@@ -77,46 +66,3 @@ pub struct MutListItem{
 }
 
 
-#[derive(Debug, PartialEq)]
-pub enum ListDef{
-    Def(ListDefObj),
-    ///ListItem内のInnerListは、InnerDefのDefaultを参照する形になる
-    InnerItem(Rc<ListDefObj>),
-}
-//
-// impl RustList{
-//     pub fn is_auto_id(&self) -> bool{
-//         match self.list_type{
-//             ListType::AutoID(_) => true,
-//             _ => false,
-//         }
-//     }
-//
-//     pub fn is_null_auto_id(&self) -> bool{
-//         match self.list_type{
-//             ListType::AutoID(val) =>{
-//                 val.is_none()
-//             },
-//             _ =>{ false }
-//         }
-//     }
-//
-//     pub fn set_auto_id(&mut self, id : u64) -> Result<(), ()>{
-//         match self.list_type{
-//             ListType::AutoID(_) =>{
-//                 self.list_type = ListType::AutoID(Some(id));
-//                 Ok(())
-//             },
-//             _=>{ Err(()) }
-//         }
-//     }
-//
-//     pub fn new() -> RustList{
-//         RustList{
-//             list_type : ListType::Normal,
-//             default : ListDef::Def(RustObject::new()),
-//             list : LinkedHashMap::new(),
-//             redef : BTreeMap::new(),
-//         }
-//     }
-// }
