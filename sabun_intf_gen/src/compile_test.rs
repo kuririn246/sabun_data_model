092279d69ccbb181a3a1704dbd2eee5df30f9f28
massive-spoiler <reddit_chan@outlook.jp>
2020-07-10 16:11:38 +0900

    IDにも@をつけられるように・・・

--- sabun_intf_gen/src/compile_test.rs
+++ sabun_intf_gen/src/compile_test.rs
@@ -22,11 +22,11 @@ pub struct RootItem {
     p_str : Option<String>,
     p_at_at_desu : Option<String>,
     p_col : Option<ColData>,
-    p_refed : Option<RefedData>,
+    p_atrefed : Option<AtrefedData>,
 }
 impl RootItem {
     pub fn new(ptr : RootObjectPtr) -> RootItem{
-        RootItem{ ptr, p_bu : None, p_str : None, p_at_at_desu : None, p_col : None, p_refed : None, }
+        RootItem{ ptr, p_bu : None, p_str : None, p_at_at_desu : None, p_col : None, p_atrefed : None, }
     }
     pub fn root(&mut self) -> *mut RootItem{ self }
 
@@ -77,13 +77,13 @@ impl RootItem {
         self.p_col = Some(ColData::new(ans, self.root()));
         return self.p_col.as_mut().unwrap();
     }
-    pub fn refed(&mut self) -> &mut RefedData{
-        if self.p_refed.is_some() {
-            return self.p_refed.as_mut().unwrap();
+    pub fn atrefed(&mut self) -> &mut AtrefedData{
+        if self.p_atrefed.is_some() {
+            return self.p_atrefed.as_mut().unwrap();
         }
-        let ans = root::get_data(self.ptr, "refed").unwrap();
-        self.p_refed = Some(RefedData::new(ans, self.root()));
-        return self.p_refed.as_mut().unwrap();
+        let ans = root::get_data(self.ptr, "@refed").unwrap();
+        self.p_atrefed = Some(AtrefedData::new(ans, self.root()));
+        return self.p_atrefed.as_mut().unwrap();
     }
 }
 
@@ -118,11 +118,11 @@ pub struct ColItem {
     pub ptr : ListItemPtr,
     pub root : *mut RootItem,
     p_nakabu : Option<bool>,
-    ref_refed : Option<*mut RefedItem>,
+    ref_atrefed : Option<*mut AtrefedItem>,
 }
 impl ColItem {
     pub fn new(ptr : ListItemPtr, root : *mut RootItem) -> ColItem{
-        ColItem{ ptr, root, p_nakabu : None, ref_refed : None, }
+        ColItem{ ptr, root, p_nakabu : None, ref_atrefed : None, }
     }
     pub fn root(&mut self) -> *mut RootItem{ self.root }
 
@@ -135,66 +135,66 @@ impl ColItem {
         self.p_nakabu = Some(ans);
         return self.p_nakabu.clone().unwrap()
     }
-    pub fn ref_refed(&mut self) -> &mut RefedItem{
-        if self.ref_refed.is_some() {
-            return unsafe{ self.ref_refed.unwrap().as_mut().unwrap() };
+    pub fn ref_atrefed(&mut self) -> &mut AtrefedItem{
+        if self.ref_atrefed.is_some() {
+            return unsafe{ self.ref_atrefed.unwrap().as_mut().unwrap() };
         }
-        let qv = list_item::get_ref(self.ptr, "refed").unwrap();
+        let qv = list_item::get_ref(self.ptr, "@refed").unwrap();
         let ref_id = if let Qv::Val(v) = qv{ v } else { unreachable!() };
         let mut root = unsafe{ self.root.as_mut().unwrap() };
-        let ref_ptr : *mut RefedItem = root.refed().from_id(ref_id).unwrap();
-        self.ref_refed = Some(ref_ptr);
-        let pp = self.ref_refed.unwrap();
+        let ref_ptr : *mut AtrefedItem = root.atrefed().from_id(ref_id).unwrap();
+        self.ref_atrefed = Some(ref_ptr);
+        let pp = self.ref_atrefed.unwrap();
         unsafe{ pp.as_mut().unwrap() }
     }
 }
 
 #[derive(Debug, PartialEq)]
-pub struct RefedData {
+pub struct AtrefedData {
     pub ptr : ConstDataPtr,
     root : *mut RootItem,
-    p_first : Option<RefedItem>,
-    p_second : Option<RefedItem>,
+    p_atsecond : Option<AtrefedItem>,
+    p_first : Option<AtrefedItem>,
 }
-impl RefedData {
-    pub fn new(ptr : ConstDataPtr, root : *mut RootItem) -> RefedData{ RefedData{ ptr, root, p_first : None, p_second : None, } }
+impl AtrefedData {
+    pub fn new(ptr : ConstDataPtr, root : *mut RootItem) -> AtrefedData{ AtrefedData{ ptr, root, p_atsecond : None, p_first : None, } }
     pub fn root(&mut self) -> *mut RootItem{ self.root }
-    pub fn first(&mut self) -> &mut RefedItem {
+    pub fn atsecond(&mut self) -> &mut AtrefedItem {
+        if self.p_atsecond.is_some() {
+            return self.p_atsecond.as_mut().unwrap();
+        }
+        let ptr = data::get_value(self.ptr, "@second").unwrap();
+        let item = AtrefedItem::new(ptr, self.root());
+        self.p_atsecond = Some(item);
+        self.p_atsecond.as_mut().unwrap()
+    }
+    pub fn first(&mut self) -> &mut AtrefedItem {
         if self.p_first.is_some() {
             return self.p_first.as_mut().unwrap();
         }
         let ptr = data::get_value(self.ptr, "first").unwrap();
-        let item = RefedItem::new(ptr, self.root());
+        let item = AtrefedItem::new(ptr, self.root());
         self.p_first = Some(item);
         self.p_first.as_mut().unwrap()
     }
-    pub fn second(&mut self) -> &mut RefedItem {
-        if self.p_second.is_some() {
-            return self.p_second.as_mut().unwrap();
-        }
-        let ptr = data::get_value(self.ptr, "second").unwrap();
-        let item = RefedItem::new(ptr, self.root());
-        self.p_second = Some(item);
-        self.p_second.as_mut().unwrap()
-    }
-    pub fn from_id(&mut self, id : &str) -> Option<&mut RefedItem> {
+    pub fn from_id(&mut self, id : &str) -> Option<&mut AtrefedItem> {
         match id{
+            "@second" => Some(self.atsecond()),
             "first" => Some(self.first()),
-            "second" => Some(self.second()),
             _ =>{ None },
         }
     }
 }
 
 #[derive(Debug, PartialEq)]
-pub struct RefedItem {
+pub struct AtrefedItem {
     pub ptr : ListItemPtr,
     pub root : *mut RootItem,
     p_mem : Option<i64>,
 }
-impl RefedItem {
-    pub fn new(ptr : ListItemPtr, root : *mut RootItem) -> RefedItem{
-        RefedItem{ ptr, root, p_mem : None, }
+impl AtrefedItem {
+    pub fn new(ptr : ListItemPtr, root : *mut RootItem) -> AtrefedItem{
+        AtrefedItem{ ptr, root, p_mem : None, }
     }
     pub fn root(&mut self) -> *mut RootItem{ self.root }
 
--- sabun_intf_gen/src/imp/to_source_from_col_temp.rs
+++ sabun_intf_gen/src/imp/to_source_from_col_temp.rs
@@ -1,6 +1,7 @@
 use crate::imp::structs::sources::StructSource;
 use crate::imp::structs::str_and_tab::{str_and_tabs_to_string, StrAndTab};
 use crate::imp::structs::struct_desc::StructDesc;
+use crate::imp::util::to_type_name::to_snake_name;
 
 pub fn to_source_from_col_temp(imp : &StructDesc) -> StructSource {
     let mut result: Vec<StrAndTab> = vec![];
@@ -10,7 +11,7 @@ pub fn to_source_from_col_temp(imp : &StructDesc) -> StructSource {
     result.push(StrAndTab::new(format!("pub ptr : {},", &imp.col_ptr_type), 1));
     result.push(StrAndTab::new(format!("root : *mut RootItem,"), 1));
     for key in &imp.keys{
-        result.push(StrAndTab::new(format!("p_{} : Option<{}>,", key.key(), &imp.item_struct_name), 1));
+        result.push(StrAndTab::new(format!("p_{} : Option<{}>,", to_snake_name(key.key()), &imp.item_struct_name), 1));
     }
     result.push(StrAndTab::new("}".to_string(), 0));
 
@@ -20,20 +21,20 @@ pub fn to_source_from_col_temp(imp : &StructDesc) -> StructSource {
                           &imp.col_ptr_type, &imp.col_struct_name, &imp.col_struct_name);
 
     for key in &imp.keys{
-        new.push_str(&format!("p_{} : None, ", key.key()));
+        new.push_str(&format!("p_{} : None, ", to_snake_name(key.key())));
     }
     new.push_str(&format!("}} }}"));
     result.push(StrAndTab::new(new, 1));
     result.push(StrAndTab::new(format!("pub fn root(&mut self) -> *mut RootItem{{ self.root }}"), 1));
 
     for key in &imp.keys{
-        let key_name = if key.is_old(){ format!("{}_old", key.key()) } else{ key.key().to_string() };
+        let key_name = if key.is_old(){ format!("{}_old", to_snake_name(key.key())) } else{to_snake_name(key.key()) };
         result.push(StrAndTab::new(
             format!("pub fn {}(&mut self) -> &mut {} {{", &key_name, &imp.item_struct_name), 1));
         result.push(StrAndTab::new(
-            format!("if self.p_{}.is_some() {{", key.key()), 2));
+            format!("if self.p_{}.is_some() {{", to_snake_name(key.key())), 2));
         result.push(StrAndTab::new(
-            format!("return self.p_{}.as_mut().unwrap();", key.key()), 3));
+            format!("return self.p_{}.as_mut().unwrap();", to_snake_name(key.key())), 3));
         result.push(StrAndTab::new(
             format!("}}"), 2));
 
@@ -42,9 +43,9 @@ pub fn to_source_from_col_temp(imp : &StructDesc) -> StructSource {
         result.push(StrAndTab::new(
             format!("let item = {}::new(ptr, self.root());", &imp.item_struct_name), 2));
         result.push(StrAndTab::new(
-            format!("self.p_{} = Some(item);", key.key()), 2));
+            format!("self.p_{} = Some(item);", to_snake_name(key.key())), 2));
         result.push(StrAndTab::new(
-            format!("self.p_{}.as_mut().unwrap()", key.key()), 2));
+            format!("self.p_{}.as_mut().unwrap()", to_snake_name(key.key())), 2));
         result.push(StrAndTab::new(
             "}".to_string(), 1));
     }
@@ -56,7 +57,7 @@ pub fn to_source_from_col_temp(imp : &StructDesc) -> StructSource {
             format!("match id{{"), 2));
         for key in &imp.keys {
             result.push(StrAndTab::new(
-                format!("\"{}\" => Some(self.{}()),", key.key(), key.key()), 3));
+                format!("\"{}\" => Some(self.{}()),", key.key(), to_snake_name(key.key())), 3));
         }
         result.push(StrAndTab::new(
             format!("_ =>{{ None }},"), 3));
