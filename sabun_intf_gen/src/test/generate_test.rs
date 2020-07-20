e02e5d95d8ef338e75d3726af41d66dbad06c8ff
reddi <reddit_chan@outlook.jp>
2020-07-21 08:20:58 +0900

    睡眠障害・二日酔いのためなかなかすすまない

--- sabun_intf_gen/src/test/generate_test.rs
+++ sabun_intf_gen/src/test/generate_test.rs
@@ -1,20 +1,20 @@
-#[cfg(test)]
-mod tests {
-    use sabun_maker::json_dir_to_rust;
-    use crate::generate_interface;
-    //use crate::rust_to_json_new_default;
-    //use crate::imp::json_to_rust::json_root_to_rust;
-    //use crate::imp::rust_to_json::root_to_json::root_to_json_new_default;
-
-    #[test]
-    fn it_works() {
-        match json_dir_to_rust("src/json_dir/test", true) {
-            Ok(a) => {
-                //println!("{:?}", a);
-                let ans = generate_interface(&a);
-                println!("{}", ans.to_string());
-            },
-            Err(e) => { println!("val 1 {}", e.message) }
-        }
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use sabun_maker::json_dir_to_rust;
+//     use crate::generate_interface;
+//     //use crate::rust_to_json_new_default;
+//     //use crate::imp::json_to_rust::json_root_to_rust;
+//     //use crate::imp::rust_to_json::root_to_json::root_to_json_new_default;
+//
+//     #[test]
+//     fn it_works() {
+//         match json_dir_to_rust("src/json_dir/test", true) {
+//             Ok(a) => {
+//                 //println!("{:?}", a);
+//                 let ans = generate_interface(&a);
+//                 println!("{}", ans.to_string());
+//             },
+//             Err(e) => { println!("val 1 {}", e.message) }
+//         }
+//     }
+// }
--- sabun_intf_gen/src/test/test2.rs
+++ sabun_intf_gen/src/test/test2.rs
@@ -1,61 +1,103 @@
-// #[cfg(test)]
-// mod tests {
-//
-//     struct Item{
-//         pub x : usize,
-//     }
-//
-//     struct Hoge{
-//         pub root : Root,
-//         pub ptr : RootPtr,
-//         pub item : Option<Item>,
-//     }
-//
-//     struct Root{
-//         pub x : usize,
-//     }
-//
-//     struct RootPtr{
-//         pub ptr : *const Root,
-//     }
-//     impl RootPtr{
-//         pub fn inner_x(&self) -> usize{
-//             let p = unsafe{ self.ptr.as_ref().unwrap() };
-//             p.x
-//         }
-//     }
-//
-//     impl Hoge{
-//
-//         pub fn new() -> Hoge {
-//             let root = Root { x: 2 };
-//             let ptr = RootPtr { ptr: &root };
-//             let item = Item{ x : 4 };
-//             let hoge = Hoge { root, ptr, item : Some(item) };
-//             hoge
-//         }
-//         pub fn root(&self) -> &Root{ &self.root }
-//         pub fn ptr(&mut self) -> &mut RootPtr{ &mut self.ptr }
-//         pub fn item(&mut self) -> &mut Item {
-//             if self.item.is_some(){
-//                 return self.item.as_mut().unwrap()
-//             } else{
-//                 return anoth2(self)
-//             }
-//
-//         }
-//
-//
-//     }
-//
-//     fn anoth2(hoge : &mut Hoge) -> &mut Item{
-//         hoge.item = Some(Item{ x : 10 });
-//         hoge.item.as_mut().unwrap()
-//     }
-//
-//     #[test]
-//     fn it_works() {
-//
-//
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    struct Item{
+        pub map : HashMap<String, String>,
+    }
+    impl Item{
+        pub fn new() -> Item{
+            let mut map : HashMap<String, String> = HashMap::new();
+            map.insert("param1".to_string(), "value_h".to_string());
+            Item{ map }
+        }
+        pub fn get(&self, key : &str) -> Option<&String>{
+            self.map.get(key)
+        }
+    }
+    struct List{
+        pub vec : Vec<Item>
+    }
+    impl List{
+        pub fn new() -> List{
+            List{ vec : vec![Item::new()] }
+        }
+        pub fn get(&self, index : usize) -> &Item{
+            self.vec.get(index).unwrap()
+        }
+    }
+
+    struct Root{
+        list : List
+    }
+    impl Root{
+        pub fn new() -> Root{
+            Root{ list : List::new() }
+        }
+    }
+
+    struct RootNormalIntf{
+        pub root : Root
+    }
+    impl RootNormalIntf{
+        pub fn new(root : Root) -> RootNormalIntf{
+            RootNormalIntf{ root }
+        }
+        pub fn list(&self) -> ListNormalIntf{
+            ListNormalIntf{ list : &self.root.list}
+        }
+    }
+
+    struct ListNormalIntf<'a>{
+        pub list : &'a List
+    }
+    impl ListNormalIntf<'_>{
+        pub fn new(list : &List) -> ListNormalIntf{
+            ListNormalIntf{ list }
+        }
+        pub fn get(&self, index : usize) -> ItemNormalIntf{
+            ItemNormalIntf::new(self.list.get(index))
+        }
+    }
+    struct ItemNormalIntf<'a>{
+        pub item : &'a Item
+    }
+    impl ItemNormalIntf<'_>{
+        pub fn new(item : &Item) -> ItemNormalIntf{
+            ItemNormalIntf{ item }
+        }
+        pub fn param1(&self) -> &String{
+            self.item.get("param1").unwrap()
+        }
+    }
+
+    struct ListMagicIntf{
+        list : *mut List,
+    }
+    impl ListMagicIntf{
+        pub fn new(list : &mut List) -> ListMagicIntf{
+            ListMagicIntf{ list }
+        }
+        pub fn get(&self, index : usize) -> ItemMagicIntf{
+
+        }
+    }
+    struct ItemMagicIntf{
+        item : *mut Item,
+    }
+    impl ItemMagicIntf{
+        
+    }
+
+
+
+    #[test]
+    fn it_works() {
+        let root = Root::new();
+        let intf = RootNormalIntf::new(root);
+        let list = intf.list();
+        let item = list.get(0);
+        println!("param1 {}", item.param1());
+
+    }
+}
