d5b74c9cf633b8020b660fc3f5c9d538327af963
reddi <reddit_chan@outlook.jp>
2020-07-31 06:58:33 +0900

    ほとんど原型を保っておらぬ

--- sabun_maker/src/imp/structs/rust_string.rs
+++ sabun_maker/src/imp/structs/rust_string.rs
@@ -1,13 +1,17 @@
 use std::fmt::{Display, Formatter};
+use std::cell::RefCell;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct RustString{
-    str : Box<String>,
+    str : RefCell<String>,
 }
 
 impl RustString{
-    pub(crate) fn new(s : String) -> RustString{ RustString{ str : Box::new(s) }}
+    pub(crate) fn new(s : String) -> RustString{ RustString{ str : RefCell::new(s) }}
     pub(crate) fn str(&self) -> &str{ self.str.as_ref().as_ref() }
+    pub(crate) fn write(&mut self, s : String){
+        self.str.replace(s);
+    }
 }
 
 impl Display for RustString{
