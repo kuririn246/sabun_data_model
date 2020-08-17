3aa4451405e8cbf4708483eb95e5ac41ad4d447d
massive-spoiler <reddit_chan@outlook.jp>
2020-08-17 18:01:52 +0900

    linkedmapの手術が続く

--- sabun_maker/src/imp/intf/mut_list.rs
+++ sabun_maker/src/imp/intf/mut_list.rs
@@ -1,43 +1,39 @@
 use crate::imp::structs::linked_m::LinkedMap;
 use crate::imp::structs::rust_list::MutListItem;
 use std::marker::PhantomData;
+use std::ops::Deref;
 
-pub struct MutIntf<'a, V>{
+#[derive(Debug)]
+pub struct MutCon<'a, V : From<&'a MutListItem>>{
     map : &'a LinkedMap<MutListItem>,
-    phantom : PhantomData<&'a V>,
+    phantom : PhantomData<*mut V>,
 }
+impl<'a, V : From<&'a MutListItem>> Deref for MutCon<'a, V>{
+    type Target = LinkedMap<MutListItem>;
 
-#[derive(Debug)]
-struct BaseItem{
-    mem : u64,
-}
-#[derive(Debug)]
-struct WrappedItem<'a>{
-    base : &'a BaseItem
-}
-impl<'a> From<&'a BaseItem> for WrappedItem<'a>{
-    fn from(base : &'a BaseItem) -> Self {
-        WrappedItem{ base }
-    }
-}
-#[derive(Debug)]
-struct List<'a, V : From<&'a BaseItem>>{
-    vec : Vec<BaseItem>,
-    phantom : PhantomData<&'a V>
-}
-
-impl<'a, V : From<&'a BaseItem>> List<'a, V>{
-    fn get(&'a self, index : usize) -> V{
-        V::from(self.vec.get(index).unwrap())
+    fn deref(&self) -> &Self::Target {
+        self.map
     }
 }
 
-#[test]
-fn test_something(){
-    let mut list : List<WrappedItem> = List{ vec : vec![], phantom : PhantomData };
-    list.vec.push(BaseItem{ mem : 1 });
-    let hoge = list.get(0);
-    println!("{:?}", hoge);
-    let hoge2 = list.get(0);
-    println!("{:?} {:?}", hoge2, hoge);
+impl<'a, V : From<&'a MutListItem>> MutCon<'a, V>{
+    pub fn new(map : &'a LinkedMap<MutListItem>) -> MutCon<V>{
+        MutCon{ map, phantom : PhantomData }
+    }
+
+    pub fn last(&self) -> Option<V>{
+        self.map.last().map(|item| V::from(item))
+    }
 }
+
+#[derive(Debug)]
+struct HogeItem<'a>{
+    item : &'a MutListItem
+}
+
+impl<'a> From<&'a MutListItem> for HogeItem<'a>{
+    fn from(item : &'a MutListItem) -> Self {
+        HogeItem{ item }
+    }
+}
+
